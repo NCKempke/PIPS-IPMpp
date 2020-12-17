34836bd519364d6dc7b0fc30f657624411f85f1d
Nils-Christian Kempke <bzfkempk@zib.de>
2020-12-17 16:37:55 +0100

    remove output agian

--- PIPS-IPM/Core/LinearSolvers/PardisoSolver/PardisoSolver/PardisoProjectSolver.C
+++ PIPS-IPM/Core/LinearSolvers/PardisoSolver/PardisoSolver/PardisoProjectSolver.C
@@ -25,7 +25,6 @@ PardisoProjectSolver::PardisoProjectSolver( SparseSymMatrix * sgm ) : PardisoSol
 #endif
 
    num_threads = PIPSgetnOMPthreads();
-   std::cout << "running on " << num_threads << "threads" << std::endl;
    solver = 0; /* sparse direct solver */
 }
 
@@ -37,7 +36,6 @@ PardisoProjectSolver::PardisoProjectSolver( DenseSymMatrix * m ) : PardisoSolver
 #endif
 
    num_threads = PIPSgetnOMPthreads();
-   std::cout << "running on " << num_threads << "threads" << std::endl;
    solver = 0; /* sparse direct solver */
 }
 
