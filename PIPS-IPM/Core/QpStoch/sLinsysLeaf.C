e5b853218fd297073e0022a765c5793e3a758b62
Nils-Christian Kempke <bzfkempk@zib.de>
2021-04-06 20:22:28 +0200

    reinforce assert

--- PIPS-IPM/Core/QpStoch/sLinsysLeaf.C
+++ PIPS-IPM/Core/QpStoch/sLinsysLeaf.C
@@ -279,9 +279,7 @@ void sLinsysLeaf::addLeftBorderKiInvBrToRes( DoubleMatrix& result, BorderBiBlock
       assert( end_cols <= nx_border + Br.n_empty_rows + myl_border + mzl_border );
    else
    {
-//         std::cout <<
-//         assert( end_cols == nx_border + Br.n_empty_rows + myl_border + mzl_border && begin_cols == 0 );
-
+         assert( end_cols == nx_border + Br.n_empty_rows + myl_border + mzl_border && begin_cols == 0 );
    }
 #endif
 
