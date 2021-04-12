baa2b7cd29614c87e28f34be030052a057bb8d63
Nils-Christian Kempke <bzfkempk@zib.de>
2021-04-12 18:59:43 +0200

    change ma57 settings

--- PIPS-IPM/Core/LinearSolvers/Ma57Solver/Ma57Solver.h
+++ PIPS-IPM/Core/LinearSolvers/Ma57Solver/Ma57Solver.h
@@ -69,10 +69,10 @@ protected:
    std::vector<double> rinfo = std::vector<double>(20);
 
    /** precision aimed for with iterative refinement */
-   const double precision = 1e-7;
+   const double precision = 1e-6;
 
-   const int max_tries = 8;
-   const int n_iterative_refinement = 10;
+   const int max_tries = 4;
+   const int n_iterative_refinement = 5;
 
    const int ooqp_print_level_warnings = 10000;
 
