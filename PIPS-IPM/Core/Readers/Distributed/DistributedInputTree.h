4c7fb5982cbb43830b386650bdd66b3a3e94ee6f
Nils-Christian Kempke <bzfkempk@zib.de>
2021-06-17 12:26:06 +0200

    remove unused constructors

--- PIPS-IPM/Core/Readers/Distributed/DistributedInputTree.h
+++ PIPS-IPM/Core/Readers/Distributed/DistributedInputTree.h
@@ -30,18 +30,7 @@ public:
       friend class DistributedTreeCallbacks;
 
    public:
-      DistributedInputNode(int id_ = -1);
-
       DistributedInputNode(int id_, int n_, int my_, int myl_, int mz_, int mzl_);
-
-      DistributedInputNode(void* user_data, int id, int n, int my, int mz, FMAT fQ, FNNZ fnnzQ, FVEC fc, FMAT fA, FNNZ fnnzA, FMAT fB, FNNZ fnnzB, FVEC fb,
-            FMAT fC, FNNZ fnnzC, FMAT fD, FNNZ fnnzD, FVEC fclow, FVEC ficlow, FVEC fcupp, FVEC ficupp, FVEC fxlow, FVEC fixlow, FVEC fxupp,
-            FVEC fixupp, bool deleteUserData = false);
-
-      // full callback constructor without constraints
-      DistributedInputNode(void* user_data, int id, FNNZ n, FNNZ my, FNNZ mz, FMAT fQ, FNNZ fnnzQ, FVEC fc, FMAT fA, FNNZ fnnzA, FMAT fB, FNNZ fnnzB,
-            FVEC fb, FMAT fC, FNNZ fnnzC, FMAT fD, FNNZ fnnzD, FVEC fclow, FVEC ficlow, FVEC fcupp, FVEC ficupp, FVEC fxlow, FVEC fixlow, FVEC fxupp,
-            FVEC fixupp, bool deleteUserData = false);
 
       // full callback constructor including linking constraints
       DistributedInputNode(void* user_data, int id, FNNZ n, FNNZ my, FNNZ myl, FNNZ mz, FNNZ mzl, FMAT fQ, FNNZ fnnzQ, FVEC fc, FMAT fA, FNNZ fnnzA,
