dd28dae49ec21f6f5ac1b677bec11db97df3184c
Nils-Christian Kempke <bzfkempk@zib.de>
2021-06-18 09:36:34 +0200

    add curtis-reid scaler files

---
+++ PIPS-IPM/Core/Preprocessing/CurtisReidScaler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by nils-christian on 18.06.21.
+//
+
+#include "CurtisReidScaler.h"
---
+++ PIPS-IPM/Core/Preprocessing/CurtisReidScaler.h
@@ -0,0 +1,14 @@
+//
+// Created by nils-christian on 18.06.21.
+//
+
+#ifndef PIPSIPMPP_CURTISREIDSCALER_H
+#define PIPSIPMPP_CURTISREIDSCALER_H
+
+
+class CurtisReidScaler {
+
+};
+
+
+#endif //PIPSIPMPP_CURTISREIDSCALER_H
