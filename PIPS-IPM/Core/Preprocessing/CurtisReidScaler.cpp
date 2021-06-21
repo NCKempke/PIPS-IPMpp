1b05d99005cf1509f4f9bdf041e9d3c661078e2b
Nils-Christian Kempke <bzfkempk@zib.de>
2021-06-21 07:21:18 +0200

    refator

--- PIPS-IPM/Core/Preprocessing/CurtisReidScaler.cpp
+++ PIPS-IPM/Core/Preprocessing/CurtisReidScaler.cpp
@@ -12,7 +12,17 @@ CurtisReidScaler::CurtisReidScaler(const ProblemFactory& problem_factory, const 
 }
 
 void CurtisReidScaler::scale() {
+   create_scaling_vectors();
 
+   auto [sum_non_zeros_columns, sum_non_zeros_equalities, sum_non_zeros_inequalities] = get_nonzero_vectors();
+
+   auto [log_sum_columns, log_sum_equalities, log_sum_inequalities] = get_log_sum_vectors();
+
+
+};
+
+
+std::tuple<std::unique_ptr<Vector<int>>, std::unique_ptr<Vector<int>>, std::unique_ptr<Vector<int>>> CurtisReidScaler::get_nonzero_vectors() const{
    std::unique_ptr<Vector<int>> sum_non_zeros_equalities = problem_factory.make_equalities_dual_integral_vector();
    std::unique_ptr<Vector<int>> sum_non_zeros_inequalities = problem_factory.make_inequalities_dual_integral_vector();
 
@@ -28,7 +38,10 @@ void CurtisReidScaler::scale() {
    this->A->getNnzPerCol(*sum_non_zeros_columns);
    this->C->getNnzPerCol(*sum_non_zeros_columns);
 
+   return {std::move(sum_non_zeros_columns), std::move(sum_non_zeros_equalities), std::move(sum_non_zeros_inequalities)};
+}
 
+std::tuple<std::unique_ptr<Vector<double>>, std::unique_ptr<Vector<double>>, std::unique_ptr<Vector<double>>> CurtisReidScaler::get_log_sum_vectors() const {
    std::unique_ptr<Vector<double>> log_sum_equalities = problem_factory.make_equalities_dual_vector();
    std::unique_ptr<Vector<double>> log_sum_inequalities = problem_factory.make_inequalities_dual_vector();
 
@@ -45,5 +58,9 @@ void CurtisReidScaler::scale() {
    this->A->sum_transform_rows(*log_sum_equalities, two_log_if_nonzero);
    this->C->sum_transform_rows(*log_sum_inequalities, two_log_if_nonzero);
 
+   this->A->sum_transform_columns(*log_sum_columns, two_log_if_nonzero);
+   this->C->sum_transform_columns(*log_sum_columns, two_log_if_nonzero);
 
-};
+   return {std::move(log_sum_columns), std::move(log_sum_equalities), std::move(log_sum_inequalities)};
+}
+
